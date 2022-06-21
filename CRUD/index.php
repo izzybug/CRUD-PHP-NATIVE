fe660f833b42093c4f950cb0623b5489037e465c
izza <65022161+izzybug@users.noreply.github.com>
2022-06-21 15:16:10 +0700

    1

---
+++ CRUD/index.php
@@ -0,0 +1,54 @@
+<?php
+// Create database connection using config file
+include_once("config.php");
+ 
+// Fetch all users data from database
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
+?>
+ 
+<html>
+<head>    
+    <title>Homepage</title>
+    <style>
+        table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+        }
+
+        td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+        }
+
+        tr:nth-child(even) {
+        background-color: #dddddd;
+        }
+        a {
+            color: black;
+            font-size: 15px;
+        }
+    </style>
+</head>
+ 
+<body>
+<button><a href="add.php">Add New User</a></button><br/><br/>
+ 
+    <table width='80%' border=1>
+ 
+    <tr>
+        <th>Name</th> <th>Mobile</th> <th>Email</th> <th>Update</th>
+    </tr>
+    <?php  
+    while($user_data = mysqli_fetch_array($result)) {         
+        echo "<tr>";
+        echo "<td>".$user_data['name']."</td>";
+        echo "<td>".$user_data['mobile']."</td>";
+        echo "<td>".$user_data['email']."</td>";    
+        echo "<td><a href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
+    }
+    ?>
+    </table>
+</body>
+</html>
